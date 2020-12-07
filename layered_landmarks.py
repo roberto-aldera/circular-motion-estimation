7b78e9e0969e15e5969376af03e74d29c909661c
Roberto Aldera <roberto@robots.ox.ac.uk>
2020-12-07 14:21:19 +0000

    Minor changes to landmark layering script for clearer visualisations

--- layered_landmarks.py
+++ layered_landmarks.py
@@ -28,10 +28,11 @@ def render_landmarks(params):
     # plt.ylim(-dim, dim)
 
     k_inner_range = 20
+    k_every_nth_scan = 25
+    plot_as_inner_and_outer_landmarks = False
 
     for i in range(params.num_samples):
         folder_path = landmarks_path + "landmarks_" + str(i)
-        print(folder_path)
         landmarks_data_from_csv_with_timestamp = np.genfromtxt(folder_path + ".csv", delimiter=",")
         landmarks_timestamp = landmarks_data_from_csv_with_timestamp[0, 0]
         raw_landmarks = landmarks_data_from_csv_with_timestamp[1:, :]
@@ -47,20 +48,22 @@ def render_landmarks(params):
 
             global_pose = global_pose @ se3s[relative_pose_index]
             landmarks = np.transpose(global_pose @ np.transpose(landmarks))
-            if i % 10 == 0:
-
-                plt.plot(global_pose[1, 3], global_pose[0, 3], 'b^')
+            if i % k_every_nth_scan == 0:
+                print("Processing index: ", i)
 
                 # plot x and y swapped around so that robot is moving forward as upward direction
-                # plt.plot(landmarks[:, 1], landmarks[:, 0], 'o', markerfacecolor='none')  # use ',' for pixel markers
-                # plt.plot(landmarks[:, 1], landmarks[:, 0], ',')  # use ',' for pixel markers
-
-                for j in range(len(landmarks)):
-                    this_landmark_range = np.sqrt(pow(raw_landmarks[j, 1], 2) + pow(raw_landmarks[j, 0], 2))
-                    if this_landmark_range < k_inner_range:
-                        plt.plot(landmarks[j, 1], landmarks[j, 0], 'r,')  # use ',' for pixel markers
-                    else:
-                        plt.plot(landmarks[j, 1], landmarks[j, 0], 'k,')  # use ',' for pixel markers
+                if plot_as_inner_and_outer_landmarks:
+                    for j in range(len(landmarks)):
+                        this_landmark_range = np.sqrt(pow(raw_landmarks[j, 1], 2) + pow(raw_landmarks[j, 0], 2))
+                        if this_landmark_range < k_inner_range:
+                            plt.plot(landmarks[j, 1], landmarks[j, 0], 'r,')  # use ',' for pixel markers
+                        else:
+                            plt.plot(landmarks[j, 1], landmarks[j, 0], 'k,')  # use ',' for pixel markers
+                    plt.plot(global_pose[1, 3], global_pose[0, 3], 'b^')
+                else:
+                    p = plt.plot(landmarks[:, 1], landmarks[:, 0], '+', markerfacecolor='none', markersize=1)
+                    # plt.plot(landmarks[:, 1], landmarks[:, 0], ',')  # use ',' for pixel markers
+                    plt.plot(global_pose[1, 3], global_pose[0, 3], '^', color=p[-1].get_color())
 
     # plot sensor range for Oxford radar robotcar dataset
     circle_theta = np.linspace(0, 2 * np.pi, 100)
@@ -69,9 +72,9 @@ def render_landmarks(params):
     x2 = global_pose[0, 3] + r * np.sin(circle_theta)
     plt.plot(x1, x2, 'g--')
 
-    x1 = global_pose[1, 3] + k_inner_range * np.cos(circle_theta)
-    x2 = global_pose[0, 3] + k_inner_range * np.sin(circle_theta)
-    plt.plot(x1, x2, 'r--')
+    # x1 = global_pose[1, 3] + k_inner_range * np.cos(circle_theta)
+    # x2 = global_pose[0, 3] + k_inner_range * np.sin(circle_theta)
+    # plt.plot(x1, x2, 'r--')
 
     plt.grid()
     plt.gca().set_aspect('equal', adjustable='box')
