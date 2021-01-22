3afefe009387a417d8103b0a7b890401f95e12da
Roberto Aldera <roberto@robots.ox.ac.uk>
2021-01-22 14:23:03 +0000

    Improve plotting for SVD on full match set, and do likewise for first ransac subset

--- toy_matchsticks.py
+++ toy_matchsticks.py
@@ -12,24 +12,39 @@ def main():
     R_offset = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
     x_coords = np.array([-1, 0, 1, 1, -1])
     y_coords = np.array([1, 1.5, 1, -1, -1])
-    P1 = np.array([x_coords, y_coords])
-    P2 = R_offset @ P1 + T_offset
+    # P1 = np.array([x_coords, y_coords])
+    # P2 = R_offset @ P1 + T_offset
 
     # Recreate P1 and P2, now with an outlier each
     P1 = np.array([np.append(x_coords, 3), np.append(y_coords, 0)])
     P2 = np.array([np.append(x_coords, -3), np.append(y_coords, -1)])
     P2 = R_offset @ P2 + T_offset
 
+    # Run SVD on all the points
+    v, theta_R = get_motion_estimate_from_svd(P1, P2, weights=np.ones(P1.shape[1]))
+    pose = [v[0], v[1], theta_R]
+    T_model = np.transpose(np.array([pose[0:2]]))
+    theta_model = pose[2]
+    # pdb.set_trace()
+
+    R_model = np.array([[np.cos(theta_model), -np.sin(theta_model)], [np.sin(theta_model), np.cos(theta_model)]])
+    P_model = R_model @ P1 + T_model
+
     matched_points = []
     plt.figure(figsize=(10, 10))
-    plt.plot(P1[0], P1[1], 'x')
-    plt.plot(P2[0], P2[1], 'x')
+    plt.plot(P1[0], P1[1], 'x', color="tab:blue")
+    plt.plot(P2[0], P2[1], 'x', color="tab:orange")
+    plt.plot(P_model[0], P_model[1], 'x', color="tab:green")
     for idx in range(P1.shape[1]):
         x1 = P1[0, idx]
         y1 = P1[1, idx]
         x2 = P2[0, idx]
         y2 = P2[1, idx]
+        x3 = P_model[0, idx]
+        y3 = P_model[1, idx]
         plt.plot([x1, x2], [y1, y2], 'k', linewidth=0.5, alpha=1)
+        plt.plot([x2, x3], [y2, y3], 'r--', linewidth=0.5, alpha=1)
+
         matched_points.append([x1, x2, y1, y2])
 
     plt.title("Proposed matches")
@@ -40,7 +55,7 @@ def main():
     plt.xlim(-dim, dim)
     plt.ylim(-dim, dim)
     plt.gca().set_aspect('equal', adjustable='box')
-    plt.savefig("/workspace/Desktop/tmp.png")
+    plt.savefig("/workspace/Desktop/full_svd.png")
     plt.close()
 
     # print(matched_points)
@@ -48,6 +63,42 @@ def main():
     # Mini RANSAC method
     pose_estimates = get_pose_estimates_with_ransac(P1, P2)
     print(pose_estimates)
+
+    # Transform points using estimate, and plot (green, hallucinated)
+    model_x_y_th = pose_estimates[0]
+    T_model = np.transpose(np.array([model_x_y_th[0:2]]))
+    theta_model = model_x_y_th[2]
+    # pdb.set_trace()
+
+    R_model = np.array([[np.cos(theta_model), -np.sin(theta_model)], [np.sin(theta_model), np.cos(theta_model)]])
+    P_model = R_model @ P1 + T_model
+
+    plt.figure(figsize=(10, 10))
+    plt.plot(P1[0], P1[1], 'x', color="tab:blue")
+    plt.plot(P2[0], P2[1], 'x', color="tab:orange")
+    plt.plot(P_model[0], P_model[1], 'x', color="tab:green")
+    for idx in range(P1.shape[1]):
+        x1 = P1[0, idx]
+        y1 = P1[1, idx]
+        x2 = P2[0, idx]
+        y2 = P2[1, idx]
+        x3 = P_model[0, idx]
+        y3 = P_model[1, idx]
+        plt.plot([x1, x2], [y1, y2], 'k', linewidth=0.5, alpha=1)
+        plt.plot([x2, x3], [y2, y3], 'r--', linewidth=0.5, alpha=1)
+
+    plt.title("Proposed matches")
+    plt.grid()
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    dim = 5
+    plt.xlim(-dim, dim)
+    plt.ylim(-dim, dim)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.savefig("/workspace/Desktop/ransac_svd.png")
+    plt.close()
+
+    # TODO: Find errors (red lines)
 
 
 def get_pose_estimates_with_ransac(P1, P2, iterations=6):
