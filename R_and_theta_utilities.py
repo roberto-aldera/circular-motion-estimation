8cefdcfd0987c05069ce6e29f091ca749da1ec8c
Roberto Aldera <roberto@robots.ox.ac.uk>
2021-02-16 09:29:41 +0000

    Add multiple landmarks example to R and theta script

---
+++ R_and_theta_utilities.py
@@ -0,0 +1,156 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pdb
+from kinematics import get_transform_by_translation_and_theta, get_transform_by_r_and_theta, \
+    get_robot_to_world_transform
+
+
+def make_plot(landmarks, start_position, end_position, radius, landmark_ranges, estimated_radius=None):
+    T_robot_to_world = get_robot_to_world_transform()
+    end_position = T_robot_to_world @ end_position
+
+    plt.figure(figsize=(10, 10))
+    plt.plot(start_position[0], start_position[1], 'ro')
+    plt.plot(end_position[0], end_position[1], 'g*')
+
+    # Draw motion circle
+    circle_theta = np.linspace(0, 2 * np.pi, 50)
+    x1 = radius + radius * np.cos(circle_theta)
+    x2 = radius * np.sin(circle_theta)
+    plt.plot(x1, x2, 'k--')
+
+    for idx in range(landmarks.shape[1]):
+        landmark_1 = T_robot_to_world @ landmarks[:, idx].reshape(-1, 1)
+
+        plt.plot(landmark_1[0], landmark_1[1], 'r^')
+
+        plt.plot([start_position[0], landmark_1[0][0]], [start_position[1], landmark_1[1][0]], 'k', linewidth=0.5,
+                 alpha=1)
+        plt.plot([end_position[0], landmark_1[0][0]], [end_position[1], landmark_1[1][0]], 'k', linewidth=0.5, alpha=1)
+
+        # Draw landmark range
+        x1 = landmark_1[0] + landmark_ranges[idx] * np.cos(circle_theta)
+        x2 = landmark_1[1] + landmark_ranges[idx] * np.sin(circle_theta)
+        plt.plot(x1, x2, 'r-.')
+
+    # Draw estimated motion circle
+    if estimated_radius:
+        x1 = estimated_radius + estimated_radius * np.cos(circle_theta)
+        x2 = estimated_radius * np.sin(circle_theta)
+        plt.plot(x1, x2, 'k--')
+
+    plt.title("Circular motion world")
+    plt.grid()
+    # dim = 5
+    # plt.xlim(-dim, dim)
+    # plt.ylim(-dim, dim)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.savefig("/workspace/data/landmark-distortion/ro_state_pb_developing/r_and_theta.png")
+    plt.close()
+
+
+def get_relative_range_and_bearing_from_x_and_y(relative_x, relative_y):
+    # Get landmark position relative to robot position in range and angle
+    relative_range = np.linalg.norm(np.array([relative_x, relative_y]))
+    relative_angle = np.arctan2(relative_y, relative_x)
+    return relative_range, relative_angle
+
+
+def get_theta_and_radius_from_single_match(d_1, d_2, phi_1, phi_2):
+    theta = 2 * np.arctan(
+        (-np.sin(phi_2) + (d_1 / d_2) * np.sin(phi_1)) / ((d_1 / d_2) * np.cos(phi_1) + np.cos(phi_2)))
+    radius = (d_2 * np.sin(phi_1 - phi_2 - theta)) / (2 * np.sin(theta / 2) * np.sin(-phi_1 + (theta / 2)))
+    return theta, radius
+
+
+def multiple_landmarks_test():
+    print("Running R and theta on toy data for multiple landmarks...")
+    k_radius = 10
+    k_theta = np.pi / 8
+    pose = get_transform_by_r_and_theta(rotation_radius=k_radius, theta=k_theta)
+    x_coords = np.array([5, 4])
+    y_coords = np.array([-2, -1])
+    start_position = np.array([0, 0])
+    end_position = [0, 0]
+    end_position = np.r_[end_position, 0, 1]  # add z = 0, and final 1 for homogenous coordinates for se3 multiplication
+    end_position = pose @ end_position
+    print("start_position:", start_position)
+    print("end_position:", end_position)
+
+    num_points = len(x_coords)
+    landmarks = np.array([x_coords, y_coords, np.zeros(num_points), np.ones(num_points)])
+    landmark_ranges = []
+
+    for idx in range(num_points):
+        landmark_1 = landmarks[:, idx].reshape(-1, 1)
+        landmark_2 = np.linalg.inv(pose) @ landmark_1
+
+        # Get landmark position relative to robot position in range and angle
+        range_1, angle_1 = get_relative_range_and_bearing_from_x_and_y(landmark_1[0], landmark_1[1])
+        range_2, angle_2 = get_relative_range_and_bearing_from_x_and_y(landmark_2[0], landmark_2[1])
+        print("Range 1:", range_1)
+        print("Range 2:", range_2)
+        print("Angle 1:", angle_1 * 180 / np.pi)
+        print("Angle 2:", angle_2 * 180 / np.pi)
+
+        landmark_ranges.append(range_2)
+
+        theta, radius = get_theta_and_radius_from_single_match(d_1=range_1, d_2=range_2, phi_1=angle_1, phi_2=angle_2)
+        print("Actual R and theta:", k_radius, ",", k_theta * 180 / np.pi)
+        print("Estimated R and theta:", radius, ",", theta * 180 / np.pi)
+
+        circular_motion_pose = get_transform_by_r_and_theta(radius, theta)
+        print("x:", circular_motion_pose[0, 3])
+        print("y:", circular_motion_pose[1, 3])
+
+    make_plot(landmarks, start_position, end_position, k_radius, landmark_ranges)
+
+
+def single_landmark_test():
+    print("Running R and theta on toy data...")
+    k_radius = 10
+    k_theta = np.pi / 8
+    pose = get_transform_by_r_and_theta(rotation_radius=k_radius, theta=k_theta)
+    x_coords = np.array([5])
+    y_coords = np.array([-2])
+    num_points = len(x_coords)
+    landmark_1 = np.array([x_coords, y_coords, np.zeros(num_points), np.ones(num_points)])
+    landmark_2 = np.linalg.inv(pose) @ landmark_1
+
+    start_position = np.array([0, 0])
+    end_position = [0, 0]
+    end_position = np.r_[end_position, 0, 1]  # add z = 0, and final 1 for homogenous coordinates for se3 multiplication
+    end_position = pose @ end_position
+
+    print("start_position:", start_position)
+    print("end_position:", end_position)
+
+    landmark_ranges = []
+
+    # Get landmark position relative to robot position in range and angle
+    range_1, angle_1 = get_relative_range_and_bearing_from_x_and_y(landmark_1[0], landmark_1[1])
+    range_2, angle_2 = get_relative_range_and_bearing_from_x_and_y(landmark_2[0], landmark_2[1])
+    # range_1, angle_1 = get_relative_range_and_bearing_from_x_and_y(-2.24, -6.22)
+    # range_2, angle_2 = get_relative_range_and_bearing_from_x_and_y(-0.1, -6.20)
+    # range_1, angle_1 = get_relative_range_and_bearing_from_x_and_y(-2.82, -6.24)
+    # range_2, angle_2 = get_relative_range_and_bearing_from_x_and_y(-0.69, -6.20)
+    print("Range 1:", range_1)
+    print("Range 2:", range_2)
+    print("Angle 1:", angle_1 * 180 / np.pi)
+    print("Angle 2:", angle_2 * 180 / np.pi)
+    landmark_ranges.append(range_2)
+
+    theta, radius = get_theta_and_radius_from_single_match(d_1=range_1, d_2=range_2, phi_1=angle_1, phi_2=angle_2)
+    print("Actual R and theta:", k_radius, ",", k_theta * 180 / np.pi)
+    print("Estimated R and theta:", radius, ",", theta * 180 / np.pi)
+
+    circular_motion_pose = get_transform_by_r_and_theta(radius, theta)
+    print("x:", circular_motion_pose[0, 3])
+    print("y:", circular_motion_pose[1, 3])
+
+    make_plot(landmark_1, start_position, end_position, k_radius, landmark_ranges, estimated_radius=radius)
+
+
+if __name__ == "__main__":
+    # single_landmark_test()
+    multiple_landmarks_test()
