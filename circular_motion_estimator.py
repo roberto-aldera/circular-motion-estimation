b633e014e535e859aa30e00c7ae12288be0c9f91
Roberto Aldera <roberto@robots.ox.ac.uk>
2021-02-19 10:21:50 +0000

    Clean up script (mostly) so debugging is less cluttered

--- circular_motion_estimator.py
+++ circular_motion_estimator.py
@@ -35,18 +35,12 @@ def circular_motion_estimation(params, radar_state_mono):
         shutil.rmtree(output_path)
     output_path.mkdir(parents=True)
 
-    # se3s, timestamps = get_ground_truth_poses_from_csv(
-    #     "/workspace/data/RadarDataLogs/2019-01-10-14-50-05-radar-oxford-10k/gt/radar_odometry.csv")
-    # # print(se3s[0])
-
-    # ro_se3s = []
-    # ro_timestamps = []
-
     poses_from_full_match_set = []
     poses_from_circular_motion = []
     timestamps_from_ro_state = []
 
     for i in range(params.num_samples):
+        # i = 3  # just for debugging because I know frame 3 is problematic
         pb_state, name_scan, _ = radar_state_mono[i]
         ro_state = get_ro_state_from_pb(pb_state)
         timestamps_from_ro_state.append(ro_state.timestamp)
@@ -69,33 +63,24 @@ def circular_motion_estimation(params, radar_state_mono):
         matched_points = []
         plt.figure(figsize=(10, 10))
 
-        # for match_idx in range(5):
         for match_idx in range(len(matches_to_plot)):
             x1 = primary_landmarks[matches_to_plot[match_idx, 1], 1]
             y1 = primary_landmarks[matches_to_plot[match_idx, 1], 0]
             x2 = secondary_landmarks[matches_to_plot[match_idx, 0], 1]
             y2 = secondary_landmarks[matches_to_plot[match_idx, 0], 0]
-            # if x1 == x2 and y1 == y2:
-            #     print("\t\t\t*** x1 == x2 and y1 == y2 for idx:", match_idx)
-            #     pdb.set_trace()
+
             plt.plot([x1, x2], [y1, y2], 'k', linewidth=0.5, alpha=1)
             matched_points.append([x1, x2, y1, y2])
 
         thetas = []
         radii = []
-        all_motions = []
 
-        x1s = []
-        y1s = []
-        x2s = []
-        y2s = []
         for tmp_idx in range(len(matched_points)):
             x1 = matched_points[tmp_idx][3]
             y1 = matched_points[tmp_idx][1]
             x2 = matched_points[tmp_idx][2]
             y2 = matched_points[tmp_idx][0]
 
-            # print(x1, y1, x2, y2)
             if x1 == x2 and y1 == y2:
                 print("\t\t\t*** x1 == x2 and y1 == y2 for idx:", tmp_idx)
             else:
@@ -104,32 +89,20 @@ def circular_motion_estimation(params, radar_state_mono):
                 theta, radius = get_theta_and_radius_from_single_match(d_1=r1, d_2=r2, phi_1=a1, phi_2=a2)
 
                 circular_motion_pose = get_transform_by_r_and_theta(radius, theta)
-                all_motions.append(circular_motion_pose)
-                # print("x:", circular_motion_pose[0, 3])
-                # print("y:", circular_motion_pose[1, 3])
 
                 thetas.append(theta)
                 radii.append(radius)
-                x1s.append(x1)
-                y1s.append(y1)
-                x2s.append(x2)
-                y2s.append(y2)
-                # pdb.set_trace()
-
-        # # Save points for debugging.
-        # with open("/workspace/data/landmark-distortion/ro_state_pb_developing/points.csv", 'w') as points_file:
-        #     wr = csv.writer(points_file, delimiter=",")
-        #     for idx in range(len(x1s)):
-        #         wr.writerow([x1s[idx], y1s[idx], x2s[idx], y2s[idx]])
 
         # find medians
         theta_median = statistics.median(thetas)
-        # radius_median = statistics.median(radii)
         print("theta median:", theta_median)
-        # print("radius median:", radius_median)
+        print("theta mean:", statistics.mean(thetas))
 
         #############################################################################
         theta_median_idx = np.argsort(thetas)[len(thetas) // 2]
+        theta_q1_idx = np.argsort(thetas)[len(thetas) // 4]
+        theta_q3_idx = np.argsort(thetas)[3 * len(thetas) // 4]
+
         print("theta median index:", theta_median_idx)
         print("theta median:", thetas[theta_median_idx])
         # Now get the theta and radius corresponding to this median value for theta:
@@ -137,8 +110,6 @@ def circular_motion_estimation(params, radar_state_mono):
         y1 = matched_points[theta_median_idx][1]
         x2 = matched_points[theta_median_idx][2]
         y2 = matched_points[theta_median_idx][0]
-
-        # print(x1, y1, x2, y2)
 
         r1, a1 = get_relative_range_and_bearing_from_x_and_y(relative_x=x1, relative_y=y1)
         r2, a2 = get_relative_range_and_bearing_from_x_and_y(relative_x=x2, relative_y=y2)
@@ -148,60 +119,31 @@ def circular_motion_estimation(params, radar_state_mono):
         poses_from_circular_motion.append([circular_motion_pose[0, 3], circular_motion_pose[1, 3],
                                            np.arctan2(circular_motion_pose[1, 0], circular_motion_pose[0, 0])])
 
-        # pdb.set_trace()
         #############################################################################
 
         # find transforms
-        # circular_motion_pose = get_transform_by_r_and_theta(rotation_radius=radius_median, theta=theta_median)
         print("x:", circular_motion_pose[0, 3])
         print("y:", circular_motion_pose[1, 3])
 
-        # pdb.set_trace()
-        # plt.figure(figsize=(10, 10))
-        # n, bins, patches = plt.hist(radii, 1000, density=False, facecolor='tab:blue')
-        plt.plot(y1s, x1s, "*")
-        plt.plot(y2s, x2s, "*")
-        plt.title("Title")
-        plt.grid()
-        # plt.gca().set_aspect('equal', adjustable='box')
-        # dim = 5
-        # plt.xlim(-dim, dim)
-        # plt.ylim(-dim, dim)
-        plt.savefig("/workspace/data/landmark-distortion/ro_state_pb_developing/debugging.pdf")
-        plt.close()
-
-        # plt.figure(figsize=(10, 10))
-        # # plt.plot(np.abs(radii), '.')
-        # n, bins, patches = plt.hist(radii, 100, density=False, facecolor='tab:blue')
-        # plt.title("Radius values")
-        # plt.grid()
-        # # plt.gca().set_aspect('equal', adjustable='box')
-        # # dim = 5
-        # # plt.xlim(0, 5000)
-        # # plt.ylim(-2000,2000)
-        # plt.savefig("/workspace/data/landmark-distortion/ro_state_pb_developing/debugging_radii.pdf")
-        # plt.close()
-
         plt.figure(figsize=(10, 10))
-        # plt.plot(np.abs(thetas), '.')
-        n, bins, patches = plt.hist(thetas, 100, density=False, facecolor='tab:blue')
+        plt.plot(np.sort(thetas), '.')
+        plt.plot(len(thetas) // 2, thetas[theta_median_idx], 'r*')
+        plt.plot(len(thetas) // 4, thetas[theta_q1_idx], 'y*')
+        plt.plot(3 * len(thetas) // 4, thetas[theta_q3_idx], 'y*')
+        # plt.hist(thetas, 100, density=False, facecolor='tab:blue')
         plt.title("Theta values")
         plt.grid()
-        # plt.gca().set_aspect('equal', adjustable='box')
-        # dim = 5
-        # plt.xlim(0, 5000)
-        # plt.ylim(-2000,2000)
-        plt.savefig("/workspace/data/landmark-distortion/ro_state_pb_developing/debugging_thetas.pdf")
+        plt.ylim(-0.05, 0.05)
+        plt.savefig(
+            "%s%s%i%s" % (figure_path, "/debugging_thetas", i, ".pdf"))
         plt.close()
 
-        # plt.figure(figsize=(10, 10))
-        # dx_vals = [motions[0, 3] for motions in all_motions]
-        # print("dx median:", statistics.median(dx_vals))
-        # plt.hist(dx_vals, 100, density=False, facecolor='tab:blue')
-        # plt.title("dx values")
-        # plt.grid()
-        # plt.savefig("/workspace/data/landmark-distortion/ro_state_pb_developing/debugging_dx_vals.pdf")
-        # plt.close()
+        plt.figure(figsize=(10, 10))
+        plt.hist(thetas, 100, density=False, facecolor='tab:blue')
+        plt.title("Theta values")
+        plt.grid()
+        plt.savefig("%s%s" % (figure_path, "/debugging_thetas_histogram.pdf"))
+        plt.close()
 
         # Motion estimate from running SVD on all the points
         P1 = []
@@ -285,7 +227,7 @@ def main():
     print("Number of indices in this radar odometry state monolithic:", len(radar_state_mono))
 
     circular_motion_estimation(params, radar_state_mono)
-    plot_csv_things(params)
+    # plot_csv_things(params)
 
 
 if __name__ == "__main__":
