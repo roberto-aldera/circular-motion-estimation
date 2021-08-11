565392552adbdd5ed6e042b194b40712fcbf5bc2
Roberto Aldera <roberto@robots.ox.ac.uk>
2021-08-11 13:52:48 +0100

    Add function to make match plot with an inset to zoom in on a specified patch

--- cme_match_plotting.py
+++ cme_match_plotting.py
@@ -53,11 +53,11 @@ def make_landmark_deltas_figure(params, radar_state_mono, results_path):
         "/workspace/data/RadarDataLogs/2019-01-10-14-50-05-radar-oxford-10k/gt/radar_odometry.csv")
     global_pose = np.eye(4)
 
-    k_start_index_from_odometry = 260
+    k_start_index_from_odometry = 370  # 260
 
     for i in range(params.num_samples):
         plt.figure(figsize=(8, 8))
-        dim = 40
+        dim = 60
         plt.xlim(-dim, dim)
         plt.ylim(-dim, dim)
         pb_state, name_scan, _ = radar_state_mono[i + k_start_index_from_odometry]
@@ -132,7 +132,7 @@ def make_landmark_deltas_figure(params, radar_state_mono, results_path):
         matches_element, = plt.plot([], [], color="k", linewidth=2.0, label="Matches")
         font_size = 16
 
-        plot_with_labels = False
+        plot_with_labels = True
         if plot_with_labels:
             plt.legend(handles=[p1_element, p2_element, matches_element, robot_element], fontsize=font_size)
             plt.title("Correspondences between two sequential landmark sets", fontsize=font_size)
@@ -143,6 +143,116 @@ def make_landmark_deltas_figure(params, radar_state_mono, results_path):
         plt.yticks(fontsize=font_size)
         plt.tight_layout()
         plt.gca().set_aspect('equal', adjustable='box')
+        plt.savefig("%s%s%i%s" % (results_path, "/matched_landmarks_subset_", i, ".pdf"))
+        plt.close()
+
+
+# Similar to function make_landmark_deltas_figure but with an added inset just to clarify a figure in the paper
+def make_landmark_deltas_figure_with_inset(params, radar_state_mono, results_path):
+    se3s, timestamps = get_ground_truth_poses_from_csv(
+        "/workspace/data/RadarDataLogs/2019-01-10-14-50-05-radar-oxford-10k/gt/radar_odometry.csv")
+    global_pose = np.eye(4)
+
+    k_start_index_from_odometry = 370
+
+    for i in range(params.num_samples):
+        plt.figure(figsize=(8, 8))
+        ax = plt.axes()
+        dim = 60
+        ax.set_xlim(-dim, dim)
+        ax.set_ylim(-dim, dim)
+
+        # Zooming bit starts here
+        from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+        from mpl_toolkits.axes_grid1.inset_locator import mark_inset
+        x_start = -10
+        x_end = -4
+        y_start = 22
+        y_end = 28
+
+        # Make the zoom-in plot:
+        axins = zoomed_inset_axes(ax, 5, loc='center right')
+        axins.set_xlim(x_start, x_end)
+        axins.set_ylim(y_start, y_end)
+        plt.xticks(visible=False)
+        plt.yticks(visible=False)
+        mark_inset(ax, axins, loc1=1, loc2=3, fc="none", ec="0.5")
+
+        pb_state, name_scan, _ = radar_state_mono[i + k_start_index_from_odometry]
+        ro_state = get_ro_state_from_pb(pb_state)
+        primary_landmarks = PbSerialisedPointCloudToPython(ro_state.primary_scan_landmark_set).get_xyz()
+        primary_landmarks = np.c_[
+            primary_landmarks, np.ones(len(primary_landmarks))]  # so that se3 multiplication works
+
+        relative_pose_index = i + k_start_index_from_odometry + 1
+        relative_pose_timestamp = timestamps[relative_pose_index]
+        print("Relative pose:", se3s[relative_pose_index])
+
+        # ensure timestamps are within a reasonable limit of each other (microseconds)
+        assert (ro_state.timestamp - relative_pose_timestamp) < 500
+
+        primary_landmarks = np.transpose(global_pose @ np.transpose(primary_landmarks))
+
+        secondary_landmarks = PbSerialisedPointCloudToPython(ro_state.secondary_scan_landmark_set).get_xyz()
+        selected_matches = get_matrix_from_pb(ro_state.selected_matches).astype(int)
+        selected_matches = np.reshape(selected_matches, (selected_matches.shape[1], -1))
+
+        print("Size of primary landmarks:", len(primary_landmarks))
+        print("Size of secondary landmarks:", len(secondary_landmarks))
+
+        matches_to_plot = selected_matches.astype(int)
+
+        matched_points = []
+        for match_idx in range(len(matches_to_plot)):
+            x1 = primary_landmarks[matches_to_plot[match_idx, 1], 1]
+            y1 = primary_landmarks[matches_to_plot[match_idx, 1], 0]
+            x2 = secondary_landmarks[matches_to_plot[match_idx, 0], 1]
+            y2 = secondary_landmarks[matches_to_plot[match_idx, 0], 0]
+            matched_points.append([x1, x2, y1, y2])
+
+        circular_motion_estimates = get_circular_motion_estimates_from_matches(matched_points)
+        pose_from_circular_motion, chosen_indices = get_dx_dy_dth_from_circular_motion_estimates(
+            circular_motion_estimates)
+
+        print("Processing index: ", i)
+        # Generate some random indices to subsample matches (figure is too dense otherwise for illustration purposes)
+        # num_subsamples = 300
+        # sample_indices = np.random.randint(low=0, high=len(matches_to_plot), size=num_subsamples)
+
+        # plot x and y swapped around so that robot is moving forward as upward direction
+        for match_idx in range(len(matches_to_plot)):  # chosen_indices:  # sample_indices:
+            # if match_idx in sample_indices:
+            # if match_idx in chosen_indices:
+            # for match_idx in chosen_indices:  # sample_indices:
+            x1 = primary_landmarks[matches_to_plot[match_idx, 1], 1]
+            y1 = primary_landmarks[matches_to_plot[match_idx, 1], 0]
+            x2 = secondary_landmarks[matches_to_plot[match_idx, 0], 1]
+            y2 = secondary_landmarks[matches_to_plot[match_idx, 0], 0]
+
+            ax.plot(x1, y1, 'o', markersize=5, markerfacecolor='none', color="tab:blue")
+            ax.plot(x2, y2, 'o', markersize=5, markerfacecolor='none', color="tab:orange")
+            ax.plot([x1, x2], [y1, y2], 'k', linewidth=2.0)
+
+            axins.plot(x1, y1, 'o', markersize=5, markerfacecolor='none', color="tab:blue")
+            axins.plot(x2, y2, 'o', markersize=5, markerfacecolor='none', color="tab:orange")
+            axins.plot([x1, x2], [y1, y2], 'k', linewidth=2.0)
+
+        robot_element, = ax.plot(0, 0, '^', markerfacecolor="tab:green", markeredgecolor="green", markersize=10,
+                                 label="Robot")
+        p1_element, = ax.plot([], [], ".", markersize=10, color="tab:blue", label="Primary landmarks")
+        p2_element, = ax.plot([], [], ".", markersize=10, color="tab:orange", label="Secondary landmarks")
+        matches_element, = ax.plot([], [], color="k", linewidth=2.0, label="Matches")
+
+        font_size = 16
+        ax.legend(handles=[p1_element, p2_element, matches_element, robot_element], fontsize=font_size)
+        ax.set_title("Correspondences between two sequential landmark sets", fontsize=font_size)
+        ax.set_xlabel("Y-Position (m)", fontsize=font_size)
+        ax.set_ylabel("X-Position (m)", fontsize=font_size)
+        ax.grid()
+        ax.tick_params(axis='x', labelsize=font_size)
+        ax.tick_params(axis='y', labelsize=font_size)
+        plt.gca().set_aspect('equal', adjustable='box')
+
         plt.savefig("%s%s%i%s" % (results_path, "/matched_landmarks_subset_", i, ".pdf"))
         plt.close()
 
@@ -256,7 +366,7 @@ def main():
     print("Results to be stored in:", results_path)
 
     # circular_motion_estimation(params, radar_state_mono, results_path)
-    make_landmark_deltas_figure(params, radar_state_mono, results_path)
+    make_landmark_deltas_figure_with_inset(params, radar_state_mono, results_path)
 
 
 if __name__ == "__main__":
